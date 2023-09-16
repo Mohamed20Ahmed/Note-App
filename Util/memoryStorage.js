c4c35795ce29c73a88855387119c2b31e06765e3
Mohamed20Ahmed <mohammedraslan942@gmail.com>
2023-09-16 22:33:26 +0300

    add memory storage file

---
+++ Util/memoryStorage.js
@@ -0,0 +1,23 @@
+const MemoryStorage = require("memorystorage");
+let store = new MemoryStorage("note-app");
+
+exports.getKeys = (store) => {
+  let keys = [];
+  for (let i = 0; i < store.length; i++) {
+    let key = store.key(i);
+    keys.push(key);
+  }
+  return keys;
+};
+
+exports.getValues = (store) => {
+  let values = [];
+  for (let i = 0; i < store.length; i++) {
+    let key = store.key(i);
+    let value = store.getItem(key);
+    values.push(value);
+  }
+  return values;
+};
+
+exports.store = store;
