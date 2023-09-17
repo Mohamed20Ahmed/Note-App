9a54a42a02a4f3efbd3c60b4ee57cd367731a5fe
Mohamed20Ahmed <mohammedraslan942@gmail.com>
2023-09-17 15:06:33 +0300

    Modify in noteRouter.js

--- router/noteRouter.js
+++ router/noteRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const noteController = require("../controller/noteController");
 
 router.get("/notes", noteController.getAllNotes);
