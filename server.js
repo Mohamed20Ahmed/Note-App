2db74aa8eb4b872e4ae533774aaa29cd49624623
Mohamed20Ahmed <mohammedraslan942@gmail.com>
2023-09-17 15:05:47 +0300

    Modify in server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
+
 const bodyparser = require("body-parser");
+
 const noteRouter = require("./router/noteRouter");
+
+const port = 3000;
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.use(cors());
 
-app.use("/api/v1", noteRouter);
+app.use("/note-app", noteRouter);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("listening on port 3000");
 });
